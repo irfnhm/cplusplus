f11dcd0894110939d8c263a1d00810212676997a
Irfan <irfnhm@gmail.com>
2018-09-02 20:16:11 +0530

    Added std::any_of

---
+++ any_of.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+    std::vector<int> foo = {0,1,-1,3,-3,5,-5};
+    if ( std::any_of(foo.begin(), foo.end(), [](int i){return i<0;}) )
+    std::cout << "There are negative elements in the range.\n";
+}
+
