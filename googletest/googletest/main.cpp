fe93429f45557464f4e565da6db632a4e1d6c847
Irfan <irfnhm@gmail.com>
2021-10-25 02:05:17 +0530

    demo GMOCK usage

--- googletest/googletest/main.cpp
+++ googletest/googletest/main.cpp
@@ -9,7 +9,11 @@
 
 #include <iostream>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "Checkout.hpp"
+
+using ::testing::_;
+using ::testing::Return;
 
 class CheckoutTest: public::testing::Test{
 public:
@@ -18,6 +22,34 @@ protected:
     Checkout checkOut;
 };
 
+class QueueInterface{
+public:
+    virtual ~QueueInterface(){;}
+    virtual void enqueue(int data) = 0;
+    virtual int dequeue() = 0;
+};
+
+class MockQueue:public QueueInterface{
+public:
+    MOCK_METHOD0(dequeue, int());
+    MOCK_METHOD1(enqueue, void(int data));
+};
+
+class DataHolder{
+public:
+    DataHolder(QueueInterface *queue):queue(queue){;}
+    void addData(int data){
+        queue->enqueue(data);
+    }
+    
+    int getData(){
+       return queue->dequeue();
+    }
+protected:
+    QueueInterface *queue;
+};
+
+//implement gtest
 TEST_F(CheckoutTest, CalculateTotal){
     checkOut.addItemPrice("a", 1);
     checkOut.addItem("a");
@@ -50,4 +82,22 @@ TEST_F(CheckoutTest, CalculateTotalWithDiscount){
 
 TEST_F(CheckoutTest, ItemWithNoPriceThrowsException){
     ASSERT_THROW(checkOut.addItem("a"), std::invalid_argument);
+}
+
+// implement gmock
+TEST(GMockTest, AddData){
+    MockQueue myMock;
+    DataHolder dh(&myMock);
+    EXPECT_CALL(myMock, enqueue(_));
+    dh.addData(1);
+}
+
+TEST(GMockTest, AddAndGetData){
+    MockQueue myMock;
+    DataHolder dh(&myMock);
+    EXPECT_CALL(myMock, enqueue(1));
+    EXPECT_CALL(myMock, dequeue()).WillOnce(Return(1));
+    dh.addData(1);
+    int data = dh.getData();
+    ASSERT_EQ(1, data);
 }
