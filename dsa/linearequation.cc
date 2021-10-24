7dba8e86bea04f24421df309d6186dd9015351ae
Irfan <irfnhm@gmail.com>
2021-10-24 11:55:12 +0530

    restructure and add gtest mocker

---
+++ dsa/linearequation.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string input;
+    cin >> input;
+
+	int a = input[0];
+   	int b = input[1];
+   	int c = input[2];
+
+    cout << a << b << c;
+}
