13ee8168ca0b0bb1577d1d3dff96356d8c6d3aea
Irfan <irfnhm@gmail.com>
2022-04-17 15:13:44 +0530

    rotate array by k times

---
+++ alorithms_data_structures/rotatearraybyk.cc
@@ -0,0 +1,11 @@
+// Leetcode Rotate array by k times
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() +k, nums.end());
+    }
+};
